12d2826380322ad912566c8ea5cab7be9fd06944
jasmeets-6575 <jasmeetsonu990@gmail.com>
2023-09-27 20:42:42 +0530

    identity contract

---
+++ by-code-eater/solidity/identity.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Main 
+{
+    string name;
+    uint age;
+
+    constructor() public 
+    {
+        name="Ravi";
+        age=17;
+    }
+
+    function getName() view public returns(string memory)
+    {
+        return name;
+    }
+    
+    function getAge() view public returns(uint)
+    {
+        return age;
+    }
+}
