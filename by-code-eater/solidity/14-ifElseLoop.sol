b737410c13aa30999cffbacfffd8f75a2f86a0f2
jasmeets-6575 <jasmeetsonu990@gmail.com>
2023-09-29 15:43:22 +0530

    if else

---
+++ by-code-eater/solidity/14-ifElseLoop.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Array 
+{
+    function check(int a) public pure returns(string memory)
+    {
+        string memory value;
+        if(a>0){
+            value = "greater than zero" ;
+        } else if(a==0){
+            value = "value is zero" ;
+         }else {
+            value = "less than zero" ;
+        }
+        return value;
+    }
+}
