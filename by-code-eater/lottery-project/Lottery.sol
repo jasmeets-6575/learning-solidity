e6414ef2211441f86782499bcd532ae360733a19
jasmeets-6575 <jasmeetsonu990@gmail.com>
2023-10-09 15:28:02 +0530

    adding select Winner function

--- by-code-eater/lottery-project/Lottery.sol
+++ by-code-eater/lottery-project/Lottery.sol
@@ -22,4 +22,14 @@ contract Lottery {
     function random() public view returns(uint){
         return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
     }
+
+    function selectWinner() public view returns(address){
+        require(msg.sender==manager);
+        require(participants.length>=3);
+        uint r=random();
+        address payable winner;
+        uint index = r % participants.length; 
+        winner= participants[index];
+        return winner;
+    }
 }
