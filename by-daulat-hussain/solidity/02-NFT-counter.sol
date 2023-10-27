e44a4fdd770e93e72e1ea8b0b62d08b76976c16e
jasmeets-6575 <jasmeetsonu990@gmail.com>
2023-10-27 13:25:33 +0530

    deleting atrifacts

--- by-daulat-hussain/solidity/02-NFT-counter.sol
+++ by-daulat-hussain/solidity/02-NFT-counter.sol
@@ -7,11 +7,9 @@ contract NFTCount{
     function count() public view returns(uint){
         return numberOfNFT;
     }
-
     function addNFT() public {
         numberOfNFT+=1;
     }
-
     function deleteNFT() public {
         numberOfNFT-=1;
     }
